bb7ba333372d7da5533130b794f522d3a97b78a1
Marquell boyd <marquellboyd@icloud.com>
2020-10-02 20:56:14 -0400

    Update grandma.rb

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,12 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma
+    if "Hi Nana, how are you ?" puts "HUH?! SPEAK UP, SONNY!"
+    elsif "HI" puts "HUH?! SPEAK UP SONNY!"
+    elsif "WHAT DID YOU EAT TODAY" puts "NO, NOT SINCE 1938!"
+    elsif "WHAT?!" puts "NO, NOT SINCE 1938!"
+    elsif "I LOVE YOU GRANDMA!" puts "I LOVE YOU TOO PUMPKIN"
+    end
+    
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
